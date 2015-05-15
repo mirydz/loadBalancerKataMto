bd1222134058e5b63cebb2a97d7d1e579da5bad8
Michal Rydz <mirydz@gmail.com>
2015-05-15 06:18:10 +0200

    test 2 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -25,6 +25,16 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
 
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(Vm.ofSize(1));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(100.0d));
+		assertThat("the server should contain vm", theServer.contains(theVm));
+	}
+
 
 	private Vm[] anEmptyListOfVms() {
 		return new Vm[0];
