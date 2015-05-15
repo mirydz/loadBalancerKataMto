b026293ebcc18dce61363bf354417f6c66c85bdc
Michal Rydz <mirydz@gmail.com>
2015-05-15 10:55:53 +0200

    test 5 green - fix

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -6,7 +6,12 @@ public class ServerLoadBalancer {
 	
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			servers[0].addVm(vm);
+			Server lessLoaded = null;
+			for(Server server : servers) {
+				if(lessLoaded == null || lessLoaded.currentLoadPercentage > server.currentLoadPercentage)
+					lessLoaded = server;
+			}
+			lessLoaded.addVm(vm);
 		}
 	}
 }
